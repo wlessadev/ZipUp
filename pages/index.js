fc86c642297b3df0e9e459a51244e3f402f8191c
Nathália Lessa <nathaliacamposlessa1@gmail.com>
2025-06-06 23:27:08 -0300

    criar botão de acesso ao cadastro pelo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
+  // Função para navegar para a página de cadastro
+  const handleRegisterClick = () => {
+    //router.push('/cadastro');
+  };
+
   return (
     <Box sx={{ textAlign: 'center', padding: 4 }}>
       <Typography variant="h3" gutterBottom>
         Bem-vindo ao ZipUp!
       </Typography>
-      <Typography variant="body1">
+      <Typography variant="body1" sx={{ marginBottom: 2 }}>
         Aproveite o seu dia e não deixe de se cadastrar para começar a usar o ZipUp!
       </Typography>
+
+      {/* Botão de Cadastro */}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleRegisterClick}
+      >
+        Cadastre-se agora
+      </Button>
     </Box>
   );
 }
