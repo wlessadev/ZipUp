6283a01640c1d0b4b325637c15e034e9de0a33cf
Nathália Lessa <nathaliacamposlessa1@gmail.com>
2025-06-08 07:00:02 -0300

    consertando a animação para evitar teleporte do apresentador

--- components/CartoonPresenter.js
+++ components/CartoonPresenter.js
@@ -9,7 +9,6 @@ export default function CartoonPresenter() {
   const waveFrames = [
     '/images/cartoon-presenter/WagnerFrames1.png',
     '/images/cartoon-presenter/WagnerFrames2.png',
-    '/images/cartoon-presenter/WagnerFrames3.png',
     '/images/cartoon-presenter/WagnerFrames4.png',
     '/images/cartoon-presenter/WagnerFrames5.png',
     '/images/cartoon-presenter/WagnerFrames6.png',
@@ -21,14 +20,14 @@ export default function CartoonPresenter() {
 
   // Índices da animação
   const animationIndices = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1
+    0,1,2,3,4,5,6,7,8,7,6,5,4,3,2,1
   ];
 
   // Animação de acenar
   useEffect(() => {
     const interval = setInterval(() => {
       setWaveFrameIndex((prev) => (prev + 1) % animationIndices.length);
-    }, 100);
+    }, 200);
     
     return () => clearInterval(interval);
   }, []);
