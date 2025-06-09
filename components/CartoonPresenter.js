178e1fd5fa3c9eff171b03bb468302e8216e979a
Nathália Lessa <nathaliacamposlessa1@gmail.com>
2025-06-09 11:03:54 -0300

    melhorando animação do apresentador

--- components/CartoonPresenter.js
+++ components/CartoonPresenter.js
@@ -5,29 +5,26 @@ import styles from '../styles/CartoonPresenter.module.css';
 export default function CartoonPresenter() {
   const [waveFrameIndex, setWaveFrameIndex] = useState(0);
   
-  // Frames da animação
+  // Frames da animação (removidos os frames 8, 9 e 10)
   const waveFrames = [
     '/images/cartoon-presenter/WagnerFrames1.png',
     '/images/cartoon-presenter/WagnerFrames2.png',
     '/images/cartoon-presenter/WagnerFrames4.png',
     '/images/cartoon-presenter/WagnerFrames5.png',
     '/images/cartoon-presenter/WagnerFrames6.png',
-    '/images/cartoon-presenter/WagnerFrames7.png',
-    '/images/cartoon-presenter/WagnerFrames8.png',
-    '/images/cartoon-presenter/WagnerFrames9.png',
-    '/images/cartoon-presenter/WagnerFrames10.png',
+    '/images/cartoon-presenter/WagnerFrames7.png'
   ];
 
-  // Índices da animação
+  // Índices da animação ajustados para os frames disponíveis (0-5)
   const animationIndices = [
-    0,1,2,3,4,5,6,7,8,7,6,5,4,3,2,1
+    0, 1, 2, 3, 4, 5, 4, 3, 2, 1
   ];
 
   // Animação de acenar
   useEffect(() => {
     const interval = setInterval(() => {
       setWaveFrameIndex((prev) => (prev + 1) % animationIndices.length);
-    }, 200);
+    }, 150);
     
     return () => clearInterval(interval);
   }, []);
