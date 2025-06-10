35b12bb78ff0416666e383d4e0d810a8fbca8eb4
Nathália Lessa <nathaliacamposlessa1@gmail.com>
2025-06-09 22:35:56 -0300

    melhorando os estilos de theme e trocando a fonte do MUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from '../pages/config/theme'; // ajuste o caminho se necessário
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -27,50 +27,99 @@ export default function Home() {
 
   return (
     <Container maxWidth="100vw" className={styles.container}>
-      <Box className={styles.contentBox}>
-        <Box className={styles.logoContainer}>
-          <Typography variant="h3" className={styles.title}>
-            Bem-vindo ao
-          </Typography>
-          <Image 
-            src="/images/Logo_ZipUp.png"
-            alt="ZipUp Logo"
-            width={120}
-            height={120}
-            style={{
-              objectFit: 'contain',
-              marginLeft: '8px'
-            }}
-          />
-          <Typography variant="h3" className={styles.title}>!</Typography>
-        </Box>
-        
-        <Typography variant="h5" gutterBottom className={styles.subtitle}>
-          Que bom te ver por aqui! Agora são{' '}
-          <Box component="span" className={styles.timeText}>
-            {isClient && currentTime ? currentTime : '--:--:--'}
-          </Box>{' '}
-          do dia{' '}
-          <Box component="span" className={styles.timeText}>
-            {isClient && currentDate ? currentDate : '--/--/----'}
-          </Box>.
-        </Typography>
-        
-        <Typography variant="body1" className={styles.description}>
-          Não deixe de se cadastrar para começar a usar o ZipUp!
-        </Typography>
+      <Box className={styles.gradientLayerOne} />
+      <Box className={styles.gradientLayerTwo} />
+      <Box className={styles.gradientLayerThree} />
+      <Box className={styles.gradientLayerFour} />
 
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          className={styles.registerButton}
-          onClick={handleRegisterClick}
-        >
-          Cadastre-se agora
-        </Button>
+      <Box className={styles.contentBox}>
+        <Box className={styles.headerBox}>
+          <Box className={styles.titleGroup}>
+            <div className={styles.titleText}>
+              Bem-vindo(a) ao&nbsp;
+            </div>
+            <Image 
+              src="/images/Logo_ZipUp.png"
+              alt="Logo do ZipUp"
+              width={120}
+              height={120}
+              style={{ objectFit: 'contain' }}
+            />
+            <div className={styles.titleText}>
+              &nbsp;!
+            </div>
+          </Box>
+
+          <Typography variant="h5" className={styles.description}>
+            Que bom te ver por aqui!
+          </Typography>
+        </Box>
+
+        <Box className={styles.dateBox}>
+          <Typography variant="h5" gutterBottom className={styles.subtitle}>
+            {isClient && currentDate && currentTime ? (
+              <>
+                Hoje é{' '}
+                <Box component="span" className={styles.timeText}>
+                  {currentDate}
+                </Box>
+                ,{' '}
+                <Box component="span" className={styles.timeText}>
+                  {currentTime}
+                </Box>
+                .
+              </>
+            ) : (
+              <>
+                Hoje é{' '}
+                <Box component="span" className={styles.timeText}>
+                  --/--/----
+                </Box>
+                ,{' '}
+                <Box component="span" className={styles.timeText}>
+                  --:--:--
+                </Box>
+                . Vamos começar?
+              </>
+            )}
+          </Typography>
+
+          <Typography variant="body" className={styles.description}>
+            Cadastre-se agora mesmo e <br/> comece a usar o ZipUp!
+          </Typography>
+        </Box>
+
+        <Box className={styles.actionBox}>
+          <div
+            variant="contained"
+            color="primary"
+            size="large"
+            className={styles.registerButton}
+            onClick={handleRegisterClick}
+          >
+            Cadastrar
+          </div>
+
+          <Box className={styles.logoContainer}>
+            <Typography variant="h6" className={styles.footerText}>
+              Um teste
+            </Typography>
+            <Image 
+              src="/images/logo-hbi-light-contrast.png"
+              alt="Logo da HBI"
+              width={60}
+              height={60}
+              style={{ objectFit: 'contain', marginLeft: '8px' }}
+            />
+          </Box>
+        </Box>
+
       </Box>
-      <CartoonPresenter/>
+
+      <Box className={styles.characterBox}>
+        <CartoonPresenter />
+      </Box>
     </Container>
+
   );
 }
